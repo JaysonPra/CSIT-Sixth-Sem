3b29c69339857b5cedd7af445c10c5962fdd7398
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-04-11 21:00:49 +0545

    made the Entity class public

--- NET Centric/Solo Practice/Combat Engine/Entities/Entity.cs
+++ NET Centric/Solo Practice/Combat Engine/Entities/Entity.cs
@@ -1,38 +1,31 @@
 namespace Combat_Engine.Entities;
 
-abstract class Entity
+abstract public class Entity
 {
-    private int _health;
-    private int _maxHealth;
-
-    public Entity(int maxHealth, string name, int armour)
-    {
-        Health = MaxHealth = maxHealth;
-        Name = name;
-        Armour = armour;
-    }
-
-    public int Armour { get; protected set; }
-
     public string Name { get; protected set; }
-    public int MaxHealth
-    {
-        get => _maxHealth;
-        protected set => _maxHealth = value > 0 ? value : 1;
-    }
+    public int Armour { get; protected set; }
+    public int MaxHealth { get; protected set; }
 
+    private int _health;
     public int Health
     {
         get => _health;
         protected set => _health = Math.Clamp(value, 0, MaxHealth);
     }
 
-    public bool IsDead => _health <= 0;
+    public Entity(int maxHealth, string name, int armour)
+    {
+        Name = name;
+        Armour = armour;
+        MaxHealth = Math.Max(1, maxHealth);
+        Health = MaxHealth;
+    }
+
+    public bool IsDead => Health <= 0;
 
     public virtual void TakeDamage(int amount)
     {
-        double damageMultiplier = 1.0 - (Armour / 100.0);
-        int finalDamage = (int)(amount * damageMultiplier);
-        Health -= finalDamage;
+        var multiplier = 1.0 - (Armour / 100.0);
+        Health -= (int)(amount * multiplier);
     }
 }
