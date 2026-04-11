dc73eae500c605e6476e29e94be951e6f9fdd36e
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-04-11 21:01:04 +0545

    Created the player class

--- NET Centric/Solo Practice/Combat Engine/Entities/Player.cs
+++ NET Centric/Solo Practice/Combat Engine/Entities/Player.cs
@@ -1,1 +1,44 @@
+using System.Diagnostics.Tracing;
+
 namespace Combat_Engine.Entities;
+
+public class Player : Entity
+{
+    private int _currentExp;
+    public int MaxExp { get; protected set; }
+    public int Level { get; protected set; }
+    public int Exp
+    {
+        get => _currentExp;
+        protected set
+        {
+            _currentExp = value;
+            if (_currentExp >= MaxExp) { LevelUp(); }
+        }
+    }
+
+    public Player(int maxHealth, string name, int armour) : base(maxHealth, name, armour)
+    {
+        Level = 1;
+        MaxExp = 100;
+        Exp = 0;
+    }
+
+    private void LevelUp()
+    {
+        Level++;
+        Exp = 0;
+        MaxExp += Level * 2;
+
+        MaxHealth += 10;
+        Health = MaxHealth;
+
+        Console.WriteLine($"Player {Name} reached Level {Level}!");
+    }
+
+    public void GainExp(int gainedExp)
+    {
+        Console.WriteLine($"You have gained {gainedExp} EXP");
+        Exp += gainedExp;
+    }
+}
