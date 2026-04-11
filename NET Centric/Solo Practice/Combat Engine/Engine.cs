a61bc164f2df79b1a31c6a3b73d062cc36a0f0cb
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-04-11 21:01:17 +0545

    Added testing for Player class

--- NET Centric/Solo Practice/Combat Engine/Engine.cs
+++ NET Centric/Solo Practice/Combat Engine/Engine.cs
@@ -7,6 +7,23 @@ class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World");
+        Player hero = new(100, "Jayson", 20);
+
+        Console.WriteLine("--- Initial Stats ---");
+        Console.WriteLine($"{hero.Name} | HP: {hero.Health} | Level: {hero.Level}");
+
+        Console.WriteLine("--- Combat Test ---");
+        int rawDamage = 10;
+        hero.TakeDamage(rawDamage);
+        Console.WriteLine($"Ouch! You got hit! HP: {hero.Health}");
+
+        Console.WriteLine("--- EXP Test ---");
+        int gainedExp = 100;
+        hero.GainExp(gainedExp);
+
+        Console.WriteLine("--- Death Test ---");
+        rawDamage = 1000;
+        hero.TakeDamage(rawDamage);
+        Console.WriteLine($"Ouch! You got hit! HP: {hero.Health}. Dead: {hero.IsDead}");
     }
 }
