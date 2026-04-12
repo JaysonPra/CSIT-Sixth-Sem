b6fc15005c9dedcb89913cc2c21298beeae27d14
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-04-12 21:05:17 +0545

    Added constructor for enemies

--- NET Centric/Solo Practice/Combat Engine/Entities/Enemy.cs
+++ NET Centric/Solo Practice/Combat Engine/Entities/Enemy.cs
@@ -1,6 +1,11 @@
 namespace Combat_Engine.Entities;
 
-class Program
+public class Enemy : Entity
 {
+    public Enemy(int maxHealth, string name, int armour, Dictionary<DamageType, double> startingResistances)
+        : base(maxHealth, name, armour)
+    {
+        Resistences = startingResistances;
+    }
 
 }
