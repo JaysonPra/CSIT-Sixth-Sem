e1fe581adc1bba7cbb221a1c621bfd20e9962fa9
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-04-13 20:14:20 +0545

    Using correct namespace for DamageType

--- NET Centric/Solo Practice/Combat Engine/Entities/Enemy.cs
+++ NET Centric/Solo Practice/Combat Engine/Entities/Enemy.cs
@@ -1,5 +1,7 @@
 namespace Combat_Engine.Entities;
 
+using Combat_Engine.Mechanics;
+
 public class Enemy : Entity
 {
     public Enemy(int maxHealth, string name, int armour, Dictionary<DamageType, double> startingResistances)
