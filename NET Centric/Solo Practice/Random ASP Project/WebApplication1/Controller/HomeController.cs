26e34edcbe5b5a3dbcf28932054cd1f63c213721
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-04-16 20:32:17 +0545

    Controller for User model

--- NET Centric/Solo Practice/Random ASP Project/WebApplication1/Controller/HomeController.cs
+++ NET Centric/Solo Practice/Random ASP Project/WebApplication1/Controller/HomeController.cs
@@ -1,9 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
 
 public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        return View();
+        var myUser = new User
+        {
+            Id = 1,
+            Name = "Jayson",
+            Email = "jaysonyt2@gmail.com"
+        };
+
+        return View(myUser);
     }
 }
