bcc610bc2054ac73cd228eb5af2432e0b865cb5e
Jayson Pradhananga <74088170+JaysonPra@users.noreply.github.com>
2026-07-13 16:17:14 +0545

    updated

--- NET Centric/ClassStuff/July13/ConsoleApp1/Generics.cs
+++ NET Centric/ClassStuff/July13/ConsoleApp1/Generics.cs
@@ -1,9 +1,34 @@
+using System.Numerics;
+
 namespace ConsoleApp1;
+
+class Calculator<T>
+{
+    public void Sum<T1, T2>(T1 a, T2 b)
+    {
+        dynamic? x = a;
+        dynamic? y = b;
+        Console.WriteLine(x + y);
+    }
+
+    public void Diff<T1, T2>(T1 a, T2 b)
+    {
+        dynamic? x = a;
+        dynamic? y = b;
+        Console.WriteLine(x - y);
+    }
+}
 
 public class Generics
 {
     public static void Main()
     {
+        Calculator<int> calc = new();
+        calc.Sum(1, 2);
+        calc.Diff(1, 2);
 
+        Calculator<float> cac = new();
+        cac.Sum(1.4f, 2.6f);
+        cac.Diff(4.5f, 2.1f);
     }
 }
--- NET Centric/ClassStuff/July13/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ NET Centric/ClassStuff/July13/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc274a2c5e7db2c6b4a4cec84c8da892307cbf87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6ec9e5a3dca3e48ef655bee40e517a50750f2b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
